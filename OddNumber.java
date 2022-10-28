b9b8df4eb3660990825cfe16361b58bf671eb530
Manjunath B <90588273+MJBS007@users.noreply.github.com>
2022-10-28 15:33:03 -0400

    Add files via upload

---
+++ OddNumber.java
@@ -0,0 +1,19 @@
+package javaprogramsbasic1;
+
+public class OddNumber {
+
+	public static void main(String[] args) {
+		int num = 12;
+		if (num>0)
+		{
+			if (num%2!=0)
+			{
+				System.out.println(num+" Odd Number");
+			}
+			else
+			{
+				System.out.println(num+ " Even Number");
+			}		
+		}
+	}
+}

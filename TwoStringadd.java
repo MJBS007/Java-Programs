08a3b21ee6f6c9da978ddcf028878ae60320939d
Manjunath B <90588273+MJBS007@users.noreply.github.com>
2022-10-31 15:28:53 -0400

    Add files via upload

---
+++ TwoStringadd.java
@@ -0,0 +1,14 @@
+package javaprogramsbasic1;
+
+import java.util.Scanner;
+
+public class TwoStringadd {
+
+	public static void main(String[] args) {
+		Scanner scr = new Scanner (System.in);
+		System.out.println("Enter your name:");
+		String name = scr.nextLine();
+		System.out.println("Ohh baby your name is " +name+" Sounds great to here!" );
+	}
+
+}
